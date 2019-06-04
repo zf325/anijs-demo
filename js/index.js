4956f64a8e42667274197dd995b8c97164a4a255
crazyzhou <fenlary.zhou@gmail.com>
2019-06-04 20:45:21 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,106 @@
+// Tip: avoid this ton of code using AniJS ;)
+
+$(function(){
+    var dataNodeHeader = $('#dataNodeHeader');
+    // when mouseover execute the animation
+    dataNodeHeader.mouseover(function(){
+      
+      // the animation starts
+      dataNodeHeader.toggleClass('rubberBand animated');
+      
+      // do something when animation ends
+      dataNodeHeader.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+       
+       // trick to execute the animation again
+        $(e.target).removeClass('rubberBand animated');
+      
+      });
+      
+    });
+
+    $(".node-2").mouseover(function(){
+      
+        // the animation starts
+        $(this).toggleClass('rubberBand animated');
+        
+        // do something when animation ends
+        $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+         
+         // trick to execute the animation again
+          $(e.target).removeClass('rubberBand animated');
+        
+        });
+        
+      });
+
+
+    var node2_marketplance = $('#node2_marketplance');
+    var node2_person = $('#node2_person');
+    var node2_nagivate = $("#node2_negative");
+    var node2_sentiment = $('#node2_sentiment');
+    var node2_company = $("#node2_company");
+
+    // when mouseover execute the animation
+    dataNodeHeader.click(function(){
+        $(".node-2").toggleClass('hide');
+    //市场
+    // the animation starts
+    node2_marketplance.toggleClass('bounceInLeft animated');
+    
+    // do something when animation ends
+    node2_marketplance.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+    
+    // trick to execute the animation again
+        $(e.target).removeClass('bounceInLeft animated');
+    
+    });
+    //个人
+    node2_person.toggleClass('bounceInDown animated');
+    
+    // do something when animation ends
+    node2_person.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+    
+    // trick to execute the animation again
+        $(e.target).removeClass('bounceInDown animated');
+    
+    });
+    
+    //负面
+    node2_nagivate.toggleClass('bounceInLeft animated');
+    
+    // do something when animation ends
+    node2_nagivate.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+    
+    // trick to execute the animation again
+        $(e.target).removeClass('bounceInLeft animated');
+    
+    });
+
+      //负面
+      node2_sentiment.toggleClass('bounceInLeft animated');
+    
+      // do something when animation ends
+      node2_sentiment.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+      
+      // trick to execute the animation again
+          $(e.target).removeClass('bounceInLeft animated');
+      
+      });
+
+
+        //公司
+        node2_company.toggleClass('bounceInUp animated');
+
+        // do something when animation ends
+        node2_company.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e){
+        
+        // trick to execute the animation again
+            $(e.target).removeClass('bounceInLUp animated');
+        
+        });
+
+
+    });
+
+    
+})
